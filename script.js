dd200653b523c235aa0d36818c9f5fa834c2b0b8
Tamta Khurtsilava <tamtakhurtsilava02@gmail.com>
2025-12-09 23:16:29 +0400

    fix-bugs

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const burger = document.querySelector(".burger");
 const xmark = document.querySelector(".xmark");
 const menu = document.querySelector(".menu");
 
-burger.addEventListener("click", ()=>{
+const closeMenu = () => {
+  menu.classList.remove("show");
+  burger.style.display = "block";
+  xmark.style.display = "none";
+};
+
+if (burger && xmark && menu) {
+  burger.addEventListener("click", () => {
     burger.style.display = "none";
     xmark.style.display = "block";
-    menu.classList.add("show")
-})
+    menu.classList.add("show");
+  });
+
+  xmark.addEventListener("click", closeMenu);
+
+  menu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", closeMenu);
+  });
+}
